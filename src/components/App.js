35995584b7b3732785a7aad95aefa107f7671c5d
lawtonB <lawton.a.browning@gmail.com>
2016-08-12 13:59:14 -0700

    lawtonbranch initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Tarot from './Tarot';
 
 export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+      <Tarot />
       </div>
     );
   }
---
+++ src/components/Tarot.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+const config = {
+  apiKey: 'AIzaSyCxj9IPD23Wsa9XRlx5z3z_qGkefRSDy1Q',
+  authDomain: 'react-tarot.firebaseapp.com',
+  databaseURL: 'https://react-tarot.firebaseio.com',    storageBucket: 'react-tarot.appspot.com',
+};
+
+const firebaseApp = firebase.initializeApp(config);
+const storage = firebaseApp.storage();
+const storageRef = storage.ref();
+const sunRef = storageRef.child('Sun_19.jpg');
+
+export default class Tarot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardNumber: '',
+      Sun_19: ['Paternal archetype', 'Cosmic father', 'Radiance', 'Brotherly love', 'Building a common work', 'Success', 'Happiness', 'Light', 'Starting couple', 'The one helps the other to cross', 'A rich harvest', 'Glory', 'Achieved awareness', 'Father who loves his children', 'Solidarity']
+    };
+
+  }
+
+  getCardDescrip() {
+    let randomDescrip = this.state.Sun_19[Math.floor(Math.random() * this.state.Sun_19.length)];
+    console.log('length is' + this.state.Sun_19.length);
+    console.log('randomdescrip is' + randomDescrip);
+    return randomDescrip;
+  }
+
+  drawCard() {
+    console.log('card number is' + this.state.cardNumber);
+    return Math.floor(Math.random() * 22);
+  }
+
+  getCardUrl() {
+    sunRef.getDownloadURL().then(function(url) {
+      return url;
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+    <div>
+
+    <h4>{this.state.cardNumber}</h4>
+    <h4>{this.state.Sun_19}</h4>
+
+    <button onClick={e => {
+      this.setState({ cardNumber: this.drawCard() });
+    }}>Draw a Card</button>
+
+    <button onClick={e => {
+      this.setState({ Sun_19: this.getCardDescrip() });
+    }}>get a new description</button>
+
+    {this.getCardUrl()}
+
+    {console.log(sunRef)}
+
+    </div>
+  );
+  }
+}
